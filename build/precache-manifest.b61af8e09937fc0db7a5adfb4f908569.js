a26e7d9e8c9946cd7bfd3687fe1d5d96b45387af
saiteja2610 <saiteja@muzigal.com>
2022-02-25 15:20:58 +0530

    Commit

---
+++ build/precache-manifest.b61af8e09937fc0db7a5adfb4f908569.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "a947eec5963b09acefd6",
+    "url": "/saiteja2610/navbar/static/css/main.7a776141.chunk.css"
+  },
+  {
+    "revision": "a947eec5963b09acefd6",
+    "url": "/saiteja2610/navbar/static/js/main.d761e209.chunk.js"
+  },
+  {
+    "revision": "3745e653c11092c5db92",
+    "url": "/saiteja2610/navbar/static/js/runtime~main.cda4c1a1.js"
+  },
+  {
+    "revision": "635d6099f878731f6d35",
+    "url": "/saiteja2610/navbar/static/css/2.cdbc789f.chunk.css"
+  },
+  {
+    "revision": "635d6099f878731f6d35",
+    "url": "/saiteja2610/navbar/static/js/2.26284d1a.chunk.js"
+  },
+  {
+    "revision": "9ac7d1fb223f2f9a1e9e",
+    "url": "/saiteja2610/navbar/static/js/3.79a04dbb.chunk.js"
+  },
+  {
+    "revision": "ce32a3217aa55a07db49b47f49ac871c",
+    "url": "/saiteja2610/navbar/static/media/web.ce32a321.png"
+  },
+  {
+    "revision": "40fc3ef7c8d5c23da7b92382e713afb6",
+    "url": "/saiteja2610/navbar/static/media/abouts.40fc3ef7.png"
+  },
+  {
+    "revision": "e64bb7f859912c3d8f4e1d448a922031",
+    "url": "/saiteja2610/navbar/static/media/development.e64bb7f8.png"
+  },
+  {
+    "revision": "ade849958754273deb8581503d76b88e",
+    "url": "/saiteja2610/navbar/static/media/App.ade84995.png"
+  },
+  {
+    "revision": "3533b7989ab0f2bcdf1fe2c6f03b9267",
+    "url": "/saiteja2610/navbar/static/media/marketing.3533b798.png"
+  },
+  {
+    "revision": "21410312ffffa1b01d5ebb50eb2415a1",
+    "url": "/saiteja2610/navbar/static/media/Logo.21410312.png"
+  },
+  {
+    "revision": "c190b0e1e7c6fba0ee455792aa56ddc2",
+    "url": "/saiteja2610/navbar/index.html"
+  }
+];
